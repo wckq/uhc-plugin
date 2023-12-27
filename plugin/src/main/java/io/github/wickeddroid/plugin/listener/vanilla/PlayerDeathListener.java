d4881f33d3ab99cdc9e2137b48604f73df75e6c6
Agus5534 <agustinmonte06@gmail.com>
2023-12-27 12:13:28 -0300

    Wicked esto no se deja en público...

--- plugin/src/main/java/io/github/wickeddroid/plugin/listener/vanilla/PlayerDeathListener.java
+++ plugin/src/main/java/io/github/wickeddroid/plugin/listener/vanilla/PlayerDeathListener.java
@@ -2,7 +2,6 @@ package io.github.wickeddroid.plugin.listener.vanilla;
 
 import io.github.wickeddroid.api.game.UhcGame;
 import io.github.wickeddroid.api.game.UhcGameState;
-import io.github.wickeddroid.plugin.hook.DiscordWebhook;
 import io.github.wickeddroid.plugin.player.UhcPlayerRegistry;
 import io.github.wickeddroid.plugin.team.UhcTeamManager;
 import net.kyori.adventure.key.Key;
@@ -70,19 +69,5 @@ public class PlayerDeathListener implements Listener {
     uhcPlayer.setAlive(false);
 
     player.setGameMode(GameMode.SPECTATOR);
-
-    final var webhook = new DiscordWebhook("https://discord.com/api/webhooks/1119722451983667280/mGiZfpqKEAMogyFVv1jhO8ipRKSngclj3kwAfO3RW5lpwC2Qi6QBsbn1n0MuTZn6yRdS");
-
-    webhook.addEmbed(new DiscordWebhook
-            .EmbedObject()
-            .setTitle("Muerte de " + player.getName())
-            .setDescription(event.getDeathMessage())
-            .setColor(Color.RED));
-
-    try {
-      webhook.execute();
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
   }
 }
