25e12e9d6b6d54c1c8890a47bb085280745c8677
Agus5534 <agustinmonte06@gmail.com>
2024-01-04 15:02:16 -0300

    Fixed a bug where world data was not deleted

--- plugin/src/main/java/io/github/wickeddroid/plugin/world/WorldGenerator.java
+++ plugin/src/main/java/io/github/wickeddroid/plugin/world/WorldGenerator.java
@@ -7,6 +7,7 @@ import org.bukkit.*;
 import team.unnamed.inject.Inject;
 import team.unnamed.inject.Singleton;
 
+import java.io.File;
 import java.io.IOException;
 
 @Singleton
@@ -48,6 +49,21 @@ public class WorldGenerator {
     Bukkit.unloadWorld(world, false);
 
     FileUtils.deleteDirectory(world.getWorldFolder());
+  }
+
+  public void removeLobbyData() {
+    worlds.blacklist().forEach(w -> {
+      var world = Bukkit.getWorld(w);
+      if(w == null) { return; }
+
+      try {
+        FileUtils.deleteDirectory(new File(world.getWorldFolder().getAbsolutePath()+"/stats/"));
+        FileUtils.deleteDirectory(new File(world.getWorldFolder().getAbsolutePath()+"/advancements/"));
+        FileUtils.deleteDirectory(new File(world.getWorldFolder().getAbsolutePath()+"/playerdata/"));
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    });
   }
 
   public void applySettings(final World world) {
