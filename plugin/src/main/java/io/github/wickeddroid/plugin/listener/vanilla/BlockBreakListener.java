ce244f4db29da322474e4e4b2914c2734888d729
Agus5534 <agustinmonte06@gmail.com>
2024-01-11 12:59:31 -0300

    Fixed a bug

--- plugin/src/main/java/io/github/wickeddroid/plugin/listener/vanilla/BlockBreakListener.java
+++ plugin/src/main/java/io/github/wickeddroid/plugin/listener/vanilla/BlockBreakListener.java
@@ -24,9 +24,10 @@ public class BlockBreakListener implements Listener {
 
     if(worlds.blacklist().contains(world.getName()) && !event.getPlayer().hasPermission(PluginUtil.OVERRIDE_LOBBY_PROTECTION_PERMISSION)) {
       event.setCancelled(true);
+      return;
     }
 
-    if (this.uhcGame.getUhcGameState() == UhcGameState.WAITING) {
+    if (this.uhcGame.getUhcGameState() == UhcGameState.WAITING && !worlds.blacklist().contains(world.getName())) {
       event.setCancelled(true);
       return;
     }
