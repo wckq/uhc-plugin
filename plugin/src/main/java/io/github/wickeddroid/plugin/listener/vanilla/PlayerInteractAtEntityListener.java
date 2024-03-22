42676a9b0296dacf49d721f20d51210f0f30c171
Agus5534 <agustinmonte06@gmail.com>
2024-03-22 13:23:16 -0300

    Spectator can now open villagers (with permissions)

--- plugin/src/main/java/io/github/wickeddroid/plugin/listener/vanilla/PlayerInteractAtEntityListener.java
+++ plugin/src/main/java/io/github/wickeddroid/plugin/listener/vanilla/PlayerInteractAtEntityListener.java
@@ -3,6 +3,7 @@ package io.github.wickeddroid.plugin.listener.vanilla;
 import org.bukkit.GameMode;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
@@ -11,18 +12,27 @@ public class PlayerInteractAtEntityListener implements Listener {
 
     @EventHandler
     public void onPlayerInteract(PlayerInteractAtEntityEvent event) {
-        if(event.getRightClicked().getType() != EntityType.PLAYER) {
-            return;
+        if(event.getRightClicked().getType() == EntityType.PLAYER) {
+            if(event.getPlayer().getGameMode() != GameMode.SPECTATOR) { return; }
+
+            var player = (Player)event.getRightClicked();
+
+            if(player.getGameMode() != GameMode.SURVIVAL) { return; }
+
+            if(!event.getPlayer().hasPermission("uhc.staff.inventory.rightclick")) { return; }
+
+            event.getPlayer().performCommand("inv " + player.getName());
         }
 
-        if(event.getPlayer().getGameMode() != GameMode.SPECTATOR) { return; }
+        if(event.getRightClicked().getType() == EntityType.VILLAGER) {
+            if(event.getPlayer().getGameMode() != GameMode.SPECTATOR) { return; }
 
-        var player = (Player)event.getRightClicked();
+            var player = event.getPlayer();
+            var villager = (Villager)event.getRightClicked();
 
-        if(player.getGameMode() != GameMode.SURVIVAL) { return; }
+            if(!event.getPlayer().hasPermission("uhc.staff.inventory.villager")) { return; }
 
-        if(!event.getPlayer().hasPermission("uhc.staff.inventory.rightclick")) { return; }
-
-        event.getPlayer().performCommand("inv " + player.getName());
+            player.openMerchant(villager, true);
+        }
     }
 }
